65bbadc58f81d99093cb5aff2048da5a19578bb6
David Potapkin <davidosimo1@gmail.com>
2020-06-17 23:31:23 +0300

    post-list, update styles and delete extra method

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IPost } from 'src/app/shared/interfaces/post.interface';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent {
   @Input() posts: IPost[];
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
