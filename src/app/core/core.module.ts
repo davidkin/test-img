d6f2b296d8add2466be9e675ce853c8981ea6269
David Potapkin <davidosimo1@gmail.com>
2020-06-17 19:59:04 +0300

    feature/toolbar, add toolbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { MatToolbarModule } from '@angular/material';
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    MatToolbarModule
+  ],
+  exports: [
+    MatToolbarModule
   ]
 })
 export class CoreModule { }
