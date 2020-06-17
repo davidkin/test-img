f8764f1ed9a583ffc2c57b12ee9f41b379a3b024
David Potapkin <davidosimo1@gmail.com>
2020-06-17 21:15:07 +0300

    post-api, add methods: - addPost(); - deletePost(); - updatePost();

--- src/app/shared/services/post-api.service.ts
+++ src/app/shared/services/post-api.service.ts
@@ -3,6 +3,8 @@ import { IPost } from '../interfaces/post.interface';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { API } from '../config';
+import { ToasterService } from './toaster.service';
+import { tap } from 'rxjs/internal/operators/tap';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +13,32 @@ export class PostApiService {
 
   constructor(
     private http: HttpClient,
+    private toastService: ToasterService
   ) { }
 
   getPosts(): Observable<IPost[]> {
     return this.http.get<IPost[]>(`${API}/posts`);
   }
+
+  addPost(post: IPost) {
+    return this.http.post(`${API}/posts/`, post)
+      .pipe(
+        tap(() => this.toastService.openSnackBar(`Post '${post.title}' added`, 'ADD NEW POST'))
+      );
+  }
+
+  deletePost(post: IPost): Observable<IPost> {
+    return this.http.delete<IPost>(`${API}/heroes/${post.id}`)
+      .pipe(
+        tap(() => this.toastService.openSnackBar(`Post '${post.id}' deleted`, 'DELETE POST'))
+      );
+  }
+
+  updatePost(post: IPost): Observable<IPost> {
+    return this.http.put<IPost>(`${API}/heroes/${post.id}`, post)
+      .pipe(
+        tap(() => this.toastService.openSnackBar(`Post '${post.title}' deleted`, 'DELETE POST'))
+      );
+  }
+
 }
