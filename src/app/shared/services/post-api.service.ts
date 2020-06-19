529de7c73e7705925d554001f19aeeb88f4c9582
David Potapkin <davidosimo1@gmail.com>
2020-06-19 09:09:28 +0300

    api, moved api ling into environment.ts

--- src/app/shared/services/post-api.service.ts
+++ src/app/shared/services/post-api.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import { IPost } from '../interfaces/post.interface';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { API } from '../config';
 import { ToasterService } from './toaster.service';
 import { tap } from 'rxjs/internal/operators/tap';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostApiService {
+  public API = environment.api;
 
   public headers = {
     'Content-Type': 'application/json'
@@ -21,25 +22,25 @@ export class PostApiService {
   ) { }
 
   getPosts(): Observable<IPost[]> {
-    return this.http.get<IPost[]>(`${API}/posts`);
+    return this.http.get<IPost[]>(`${this.API}/posts`);
   }
 
   addPost(post: IPost): Observable<IPost> {
-    return this.http.post<IPost>(`${API}/posts/`, post, { headers: this.headers })
+    return this.http.post<IPost>(`${this.API}/posts/`, post, { headers: this.headers })
       .pipe(
         tap(() => this.toastService.openSnackBar(`Post '${post.title}' added`, 'ADD NEW POST'))
       );
   }
 
   deletePost(post: IPost): Observable<IPost> {
-    return this.http.delete<IPost>(`${API}/posts/${post.id}`)
+    return this.http.delete<IPost>(`${this.API}/posts/${post.id}`)
       .pipe(
         tap(() => this.toastService.openSnackBar(`Post '${post.title}' deleted`, 'DELETE POST'))
       );
   }
 
   updatePost(post: IPost): Observable<IPost> {
-    return this.http.patch<IPost>(`${API}/posts/${post.id}`, post, { headers: this.headers })
+    return this.http.patch<IPost>(`${this.API}/posts/${post.id}`, post, { headers: this.headers })
       .pipe(
         tap(() => this.toastService.openSnackBar(`Post '${post.title}' update`, 'UPDATE POST'))
       );
